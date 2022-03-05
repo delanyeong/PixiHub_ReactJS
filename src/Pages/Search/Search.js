c746e4666c1093723068eb6d160d200f6aa9e97f
delan <delan.april@gmail.com>
2022-03-06 02:57:05 +0800

    included search bar

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,37 @@
+import { Button, createTheme, TextField, ThemeProvider } from '@material-ui/core'
 import React from 'react'
+import SearchIcon from "@material-ui/icons/Search"
+import { useState } from 'react';
 
 function Search () {
+  const [type, setType] = useState(0);
+
+  const darkTheme = createTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#fff",
+      }
+    }
+  })
+  
   return (
     <div>
-        <span className="pageTitle">Search</span>
+      <ThemeProvider theme={darkTheme}>
+        <div style={{ dispaly: "flex", margin: "15px 0px" }}>
+          <TextField
+            style = {{ flex: 1 }}
+            className="searchBox"
+            label="Search"
+            variant="filled"
+            // onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button
+          variant="contained"
+          style={{ marginLeft: 10 }}> <SearchIcon /> 
+          </Button>
+        </div>
+      </ThemeProvider>
     </div>
   )
 }
